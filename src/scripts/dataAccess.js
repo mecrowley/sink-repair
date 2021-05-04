c86f6ab841f6491eb08a075c9d5e537b25ed086f
Madeline Crowley <madelinecrowley@gmail.com>
2021-05-04 15:51:15 -0500

    added functions to submit service request form

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -19,3 +19,19 @@ export const getRequests = () => {
     return [...applicationState.requests]
 }
 
+export const sendRequest = (userServiceRequest) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userServiceRequest)
+    }
+
+
+    return fetch(`${API}/requests`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+
+        })
+}
